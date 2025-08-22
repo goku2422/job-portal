08aa98a78d1850ac5450e88933da606361d1777d
SurajChoudhary <sclaptop4321@gmail.com>
2025-08-22 22:49:44 +0530

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(
+      `Connected To Mongodb Database ${mongoose.connection.host}` 
+    );
+  } catch (error) {
+    console.log(`MongoDB Error ${error}`);
+  }
+};
+
+export default connectDB;
+
